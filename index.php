cfc9455027dd02b2998b5bbebea47035fc84010e
Brenton MOODLEY <bmoodley@e4r16p8.wethinkcode.co.za>
2017-10-19 13:27:37 +0200

    successfully establishes db connection with error feedback

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Database connection</title>
+  </head>
+  <body>
+    <h2>
+      User Authentication System
+    </h2><hr>
+    <?php include_once './database/database.php' ?>
+  </body>
+</html>
